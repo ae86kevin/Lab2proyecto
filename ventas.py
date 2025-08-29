0e3f9a779052f56bf60be0a1761d85c053d121d2
ltkfa <ae86mm@gmail.com>
2025-08-29 00:17:40 -0600

    12(utilice IA para poder objtener informacion del emplado y del producto )

--- ventas.py
+++ ventas.py
@@ -12,22 +12,62 @@ class ventas:
         if idEmpleado in self.empleados:
             print("El empelado no existe")
             return
-        empleado=self.empleados[idEmpleado]
-        print(f"empleado: {empleado.nombre}")
 
-        nit=input("Ingrese el nit: ")
+        empleado=self.empleados[idEmpleado]
+        print(f"empleado: {empleado.nombreEmpleado}")
+
+        nit =int(input("Ingrese el nit: "))
         if nit not in self.clientes:
-            opcion = input("Cliente no registado")
-            print("Desea Registar s/n")
+            print("Clinte no registraod")
+            opcion =input("Quire registar al clietne S/N: ").lower()
             if opcion == "s":
-                nuevoCliente =clientes.registroClientes()
+                nuevoCliente=clientes.registroClientes()
                 self.clientes[nit]=nuevoCliente
             else:
-                nit = "CF"
+                nit ="CF"
                 if "CF" not in self.clientes:
-                    self.clientes[nit]=clientes.Cliente("consumidor final")
+                    self.clientes[nit]={"nombre": "consumidor final"}
         cliente=self.clientes[nit]
-        print(f"Cliete: {cliente.nombre}")
+        print(f"cliente: {cliente['nombre']}")
+
+        id_producto=int(input("Ingrese el ID del producto: "))
+        if id_producto not in self.productos:
+            print("Producto no existe")
+            return
+
+        producto=self.productos[id_producto]
+        print(f"producto: {producto['nombre']}")
+
+
+        try:
+            cantidad=int(input("cantidad vendida: "))
+            if cantidad > producto["stock"]:
+                print("no hay suficiente en stock")
+                return
+        except ValueError:
+            print("cantidad invalido")
+            return
+
+        subtotal =cantidad*producto["Precio"]
+
+        producto["Stock"]-=cantidad
+        producto["TotalVentas"]+= cantidad
+
+        venta = {
+            "Empleado": empleado.nombreEmpleado,
+            "Cliente": cliente["Nombre"],
+            "Producto": producto["Nombre"],
+            "Cantidad": cantidad,
+            "Subtotal": subtotal
+        }
+        self.ventasRealizadas.append(venta)
+
+        print(" Venta registrada ")
+        print(f"Total a pagar: Q.{subtotal:.2f}")
+
+
+
+
 
 
 
