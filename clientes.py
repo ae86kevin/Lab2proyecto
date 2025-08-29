0696ddb74ee5c95b06b6eb07b72766ca3eef160a
ltkfa <ae86mm@gmail.com>
2025-08-28 23:37:25 -0600

    11 (con ayuda de IA para agregar cargar clientes)

--- clientes.py
+++ clientes.py
@@ -1,30 +1,48 @@
 class Clientes:
-    def __int__(self):
+    def __init_(self):
         self.clientes = {}
         self.cargarClientes()
 
     def cargarClientes(self):
         try:
             with open("clientes.txt", "r",encoding="utf-8") as archivo:
-                for linea in archivo
+                for linea in archivo:
                     linea = linea.strip()
                     if linea:
-                        nit,nombre,direccion,tele
+                        nit,nombre,direccion,telefono,correo= linea.split(",")
+                        self.clientes[int(nit)] = {
+                            "Nombre": nombre,
+                            "Direccion": direccion,
+                            "telefono": telefono,
+                            "Correo": correo
+                        }
+        except FileNotFoundError:
+            print("no existe el archivo")
+            with open("clientes.txt", "w",encoding="utf-8"):
+                pass
+
+    def guardarClientes(self):
+        with open("clientes.txt", "w",encoding="utf-8") as archivo:
+            for nit,datos in self.clientes.items():
+                linea = f"{nit},{datos['Nombre']},{datos['Direccion']},{datos['Telefono']},{datos['Correo']}\n"
+                archivo.write(linea)
 
 
-
-   def registroClientes(self):
+def registroClientes(self):
         while True:
             try:
                 nit = int(input("NIT: "))
                 if nit == "":
                     print("NIT no puede estar vacio")
                     continue
-                if nit in self.DiccClientes:
+                if nit in self.clienes:
                     print("El cliente ya existe")
                     continue
+
             except ValueError:
                 print("NIT unicamente son numers")
+
+
 
         nombre = input("Nombre: ")
         if nombre == "":
@@ -43,14 +61,14 @@ class Clientes:
             print("Correo no puede estar vacio")
 
 
-        self.clienes[nit]={
+        self.clientes[nit]={
             "Nombre":nombre,
             "Direccion":direccion,
             "Telefono":telefono,
             "Correo":correo
         }
 
-        self.guardar_clientes()
+        self.guardarClienes()
         print("cliente registrado")
 
 
